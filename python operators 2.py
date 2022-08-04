7555748144dee14911f9c4c1be09d548e0c42e94
Reddy Chandra <reddy.chandra@mphasis.com>
2022-08-04 13:22:51 +0530

    firrst commit

---
+++ python operators 2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 19:16:50 2022
+
+@author: reddy.chandra
+"""
+
+a="hi this is tharun"
+print(a.split(" "))
+print(a[0])
+print(len(a))
+print(a[16])
+
+print(a[0:10])
+print(a[11:17])
+print(a[0:])
+print(a[len(a)-1])
+print(a[-1])
+
+b="mphasis"
+print(b.casefold())
+
+print(b[0:7:2])
+print(b[::1])
+print(b[::-1])
---
+++ python operators.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Aug  3 14:00:48 2022
+
+@author: reddy.chandra
+"""
+
+a=10
+b=40
+print("{}+{}={}".format(a,b,a+b))
+print("{}-{}={}".format(a,b,a-b))
+print("{}*{}={}".format(a,b,a*b))
+print("{}/{}={}".format(a,b,a/b))
+print("{}power{}={}".format(a,b,a**b))
+print("{}%{}={}".format(a,b,a%b))
+print("{}-{}={}".format(30,100,30-100))
+
+c="mphasis"
+c= c + ".......... "
+d="the Next applied..."
+print(c,d,sep=('???'))
+e=(c+' '+d)
+print(e)
+print(type(c))
+
+print(id(a))
+
+print(c,end=(';;;;'))
+print(c,d, sep=('  ')
+print("{:3d} and {:4d}".format(a,b))
+print(c.capitalize())
+print(c.upper())
+print(len(d))
+f=' '
+print(type(f))
+g=None
+print(type(g))
